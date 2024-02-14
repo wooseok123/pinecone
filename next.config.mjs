ca14e349db5e4a5e6d84b8b5a712b454267ecff7
wooseok <hosmimam@naver.com>
2024-02-14 20:50:24 +0900

    add asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
   reactStrictMode: false,
   output: "export",
   basePath: "/pinecone",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://wooseok123.github.io/pinecone"
+      : "",
 };
 
 export default nextConfig;
