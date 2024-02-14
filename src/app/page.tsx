b1f941208cf187addd2d1ca02e74d5ee0223e518
wooseok <hosmimam@naver.com>
2024-02-14 20:54:05 +0900

    add prefix to image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { prefix } from "@/@utils/config";
 
 export default function Home() {
   return (
@@ -17,7 +18,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src={`${prefix}/vercel.svg`}
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +32,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={`${prefix}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
